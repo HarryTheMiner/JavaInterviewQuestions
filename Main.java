907329dee67c10d8178a85e236e9f49bb6124786
HarryTheMiner <abhishekbohra2004@gmail.com>
2025-06-29 15:53:12 +0530

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,30 @@
+package constructorchaining;
+
+class Person
+{
+    String name,address;
+    int age;
+    public Person(int age,String name,String address)
+    {
+        this.age = age;
+        this.name = name;
+        this.address = address;
+    }
+}
+class Employe extends Person
+{
+    float salary;
+    public Employe(int age,String name ,String address,float salary)
+    {
+        super(age,name,address);
+        this.salary = salary;
+    }
+}
+public class Main
+{
+    public static void main(String[] args)
+    {
+        Employe e = new Employe(22,"Mukesh","Jaunpur",1520000);
+        System.out.println(e.name +" | "+e.salary+" | "+e.age+" | "+e.address);
+    }
+}
