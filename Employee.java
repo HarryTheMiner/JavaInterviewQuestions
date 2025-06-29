532a97d4f61bd37d80b3055d9ce3e04dfb860bdf
unknown <abhishekbohra2004@gmail.com>
2025-06-29 15:12:54 +0530

    Add Employee.java

---
+++ Employee.java
@@ -0,0 +1,30 @@
+package constructorchaining;
+
+public class Employee
+{
+    int id,age;
+    String name,address;
+
+    public Employee(int age)
+    {
+        this.age = age;
+    }
+    public Employee(int id , int age)
+    {
+        this(age);
+        this.id = id;
+    }
+    public Employee(int id,int age,String name , String address)
+    {
+        this(id,age);
+        this.name = name;
+        this.address = address;
+    }
+    public static void main(String[] args)
+    {
+        Employee e = new Employee(101,25,"Abhishek","Mayapur Pali");
+        System.out.println(e.id +" | "+e.age+" | "+e.name+" | "+e.address);
+
+
+    }
+}
